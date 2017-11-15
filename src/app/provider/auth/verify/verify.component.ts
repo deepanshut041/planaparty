b0ac70ada0d2220e5dfd16eb0d469744062a43a0
deepanshut041 <deepanshut041@gmail.com>
2017-11-15 22:48:39 +0530

    frontend design of verify component made nowworking on its backend

--- src/app/provider/auth/verify/verify.component.ts
+++ src/app/provider/auth/verify/verify.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit} from "@angular/core";
 import { AuthService } from "../auth.service";
+import { validateConfig } from "@angular/router/src/config";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-verify-admin",
@@ -8,8 +10,48 @@ import { AuthService } from "../auth.service";
 })
 
 export class VerifyComponent implements OnInit {
-  constructor(private _authService:AuthService) { 
+  alertMsg;
+  constructor(private _authService:AuthService,private router:Router ) { 
   }
   ngOnInit() {
+  }
+
+  showForm(){
+    var elem1 = document.getElementById("resendVerificationButton");
+    elem1.className = "verify-button-box hide";
+    var elem2 = document.getElementById("verifyResetEmailButton");
+    elem2.className = "verify-button-box hide";
+    var elem3 = document.getElementById("verifyResetEmail");
+    elem3.className="input-group verify-button-box show";
+  }
+
+  hideForm(){
+    var elem1 = document.getElementById("resendVerificationButton");
+    elem1.className = "verify-button-box display";
+    var elem2 = document.getElementById("verifyResetEmailButton");
+    elem2.className = "verify-button-box display";
+    var elem3 = document.getElementById("verifyResetEmail");
+    elem3.className="input-group verify-button-box hide";
+  }
+
+  sendForm(){
+    this.alertMsg = "Changed your Email";
+    this.verifyAlert();
+    this.hideForm()
+  }
+  resendEmail(){
+    this.alertMsg = "Verification Email Resend";
+    this.verifyAlert();
+  }
+
+  verifyAlert(){
+    var elem1 = document.getElementById("verifyEmailBody");
+    elem1.className = "panel-body text-center hide";
+    var elem2 = document.getElementById("verifyEmailAlert");
+    elem2.className = "text-center display";
+    setTimeout(() => {
+      elem1.className = "panel-body text-center display";
+      elem2.className = "text-center hide";
+    }, 2000);
   }
 }
