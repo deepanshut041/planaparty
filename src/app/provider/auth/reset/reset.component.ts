b9829225c251cf8bfa80e274fd12c1a0ed73c62f
deepanshut041 <deepanshut041@gmail.com>
2017-11-16 13:53:13 +0530

    ui changes change made now working on verify password with old password

--- src/app/provider/auth/reset/reset.component.ts
+++ src/app/provider/auth/reset/reset.component.ts
@@ -8,8 +8,82 @@ import { AuthService } from "../auth.service";
 })
 
 export class ResetComponent implements OnInit {
+  alertMsg:String="";
   constructor(private _authService:AuthService) { 
   }
   ngOnInit() {
   }
+  verifyAlert() {
+    this.hideVerifyBody();
+    var elemIcon = document.getElementById("verifyEmailAlertIcon");
+    elemIcon.className = "verify-alert-icon";
+    var elemMsg = document.getElementById("verifyEmailAlertMsg");
+    elemMsg.className = "verify-alert-message";
+    var elem2 = document.getElementById("verifyEmailAlert");
+    elem2.className = "text-center display";
+    setTimeout(() => {
+      this.showVerifyBody();
+      elem2.className = "text-center hide";
+    }, 2000);
+  }
+
+  errorAlert() {
+    this.hideVerifyBody();
+    var elemIcon = document.getElementById("verifyEmailAlertIcon");
+    elemIcon.className = "verify-alert-error-icon";
+    var elemMsg = document.getElementById("verifyEmailAlertMsg");
+    elemMsg.className = "verify-alert-error-message";
+    var elem2 = document.getElementById("verifyEmailAlert");
+    elem2.className = "text-center display";
+    setTimeout(() => {
+      this.showVerifyBody();
+      elem2.className = "text-center hide";
+    }, 2000);
+  }
+
+  showSpinner() {
+    let elem = document.getElementById("spinner")
+    elem.className = "text-center spinner";
+  }
+
+  hideSpinner() {
+    let elem = document.getElementById("spinner")
+    elem.className = "text-center spinner hide";
+  }
+
+  showVerifyBody() {
+    var elem1 = document.getElementById("resetPasswordBody");
+    elem1.className = "panel-body text-center display";
+  }
+
+  hideVerifyBody() {
+    var elem1 = document.getElementById("resetPasswordBody");
+    elem1.className = "panel-body text-center hide";
+  }
+
+  submitForm(Oldassword,newPassword,confirmPassword) {
+    if (newPassword === confirmPassword) {
+      this.hideVerifyBody();
+      this.showSpinner();
+      this._authService.getUser().updatePassword(newPassword).then(() => {
+        this._authService.getUser().updatePassword(newPassword).then(()=>{
+          this.alertMsg = "Changed your Password";
+          this.hideSpinner();
+          this.verifyAlert();
+        }).catch((err)=>{
+          this.alertMsg = err.message;
+          this.hideSpinner();
+          this.errorAlert();
+        })
+      }).catch((err)=>{
+        this.alertMsg = err.message;
+        this.hideSpinner();
+        this.errorAlert();
+      })
+    }
+    else{
+
+    }
+  }
+
 }
